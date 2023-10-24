f68d46e294ad6625c51b80ddf6e69048e933d5d8
Yehor <egorwhite06@gmail.com>
2023-10-24 16:09:42 +0300

    Lab7 first commit

---
+++ suma1/suma1/suma1.cpp
@@ -0,0 +1,24 @@
+﻿#include <windows.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float y, x, sum;
+    cout << "We have a simple function. Input X below to solve it.\n" << "X = ";
+    cin >> x;
+
+    sum = 0;
+    if (x != -2) {
+        for (int i = 0; i < 11; i++) {
+            sum += pow(i, 2);
+        }
+        y = (sum + sin(x)) / (x + 2);
+        cout << "The result of solution = " << y;
+    }
+    else {
+        cout << "X must not equals -2";
+    }
+}
+
